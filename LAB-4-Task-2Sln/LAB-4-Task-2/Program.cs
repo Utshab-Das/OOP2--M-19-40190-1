4d8a248614f63e0af439cd6afb9fd97f6f465f41
Utshab-Das <utshab.aiub@gmail.com>
2021-02-15 10:44:28 +0600

    Today_Lab-4

---
+++ LAB-4-Task-2Sln/LAB-4-Task-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_4_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 8, 9, 2, 4, 1, 6, 5, 3, 3, 3, 12, 18, 11, 10, 33  };
+            Console.WriteLine("\n Myarray: [{0}]", string.Join(", ", numbers));
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
---
+++ LAB-4-Task1Sln/LAB-4-Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_4_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            int[] a = new int[100];
+            Console.Write("Input the number of elements to store in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the number of elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(" {0} : ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+               
+            }
+            Console.WriteLine(" The values store into the array are : ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("{0}  ", a[i]);
+            }
+
+            Console.WriteLine(" The values store into the array in reverse are : ");
+            for (i = n - 1; i >= 0; i--)
+            {
+                Console.Write("{0} ", a[i]);
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
